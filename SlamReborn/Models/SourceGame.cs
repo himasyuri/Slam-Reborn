af949e44e23f5677caf62890db5abc12f6229996
himasyuri <v.egor36.2021@gmail.com>
2023-04-27 01:15:36 +0300

    models was added

---
+++ SlamReborn/Models/SourceGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace SlamReborn.Models
+{
+    public class SourceGame
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Directory { get; set; } = string.Empty;
+
+        public string LibraryName { get; set; } = string.Empty;
+
+        public string ToCfg { get; set; } = string.Empty;
+
+        public bool VoiceFadeOut { get; set; } = true;
+
+        public int Channels { get; } = 1;
+
+        public string FileExtension { get; } = ".wav";
+
+        public int SampleRate { get; set; } = 11025;
+
+        public int Bits { get;} = 16;
+
+        public string Exename { get; set; } = string.Empty;
+
+        public int PollInterval { get; } = 100;
+
+        public List<Track> Tracks { get; set; } = new List<Track>();
+
+        public List<string> BlackList { get; set; } = new List<string>() { "slam", "slam_listtracks", "list",
+            "tracks", "la", "slam_play", "slam_play_on",
+            "slam_play_off", "slam_updatecfg", "slam_curtrack",
+            "slam_saycurtrack", "slam_sayteamcurtrack" };
+    }
+}
---
+++ SlamReborn/Models/Track.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SlamReborn.Models
+{
+    public class Track
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public List<string> Tags { get; set; } = new List<string>();
+
+        public string HotKey { get; set; } = string.Empty;
+
+        public int Volume { get; set; } = 100;
+
+        public int StartPos { get; set; }
+
+        public int EndPos { get; set; }
+    }
+}
