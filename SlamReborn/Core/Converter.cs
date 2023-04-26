7d56a7f36e238dbeb7ccd784cc56903555af2326
himasyuri <v.egor36.2021@gmail.com>
2023-04-27 01:17:42 +0300

    Converter was added

---
+++ SlamReborn/Core/Converter.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Xabe.FFmpeg;
+
+namespace SlamReborn.Core
+{
+    public class Converter
+    {
+        public async Task ConvertToWavAsync(string trackFile, string outputFile)
+        {
+            //maybe edit paths
+            FFmpeg.SetExecutablesPath("..\\..\\..\\ffmpeg");
+            //string inputFile = $"..\\..\\..\\Music\\{trackFile}" + "." + "mp3";
+            //string outputFile = $"..\\..\\..\\..\\{trackFile}" + "." + "wav";
+
+            string inputFile = trackFile;
+            string trackName = Path.GetFileNameWithoutExtension(inputFile);
+            var snippet = await FFmpeg.Conversions.FromSnippet.Convert(inputFile, outputFile);
+            await snippet.Start();
+        }
+    }
+}
