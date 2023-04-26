2f87c3e0b52295dd8d704aaac28c0a68390762df
himasyuri <v.egor36.2021@gmail.com>
2023-04-27 01:22:47 +0300

    simple custom logger

---
+++ SlamReborn/Core/Logger.cs
@@ -0,0 +1,21 @@
+﻿using SlamReborn.Properties;
+using System;
+using System.IO;
+
+namespace SlamReborn.Core
+{
+    public class Logger
+    {
+        public void LogError(Exception ex)
+        {
+            if (Settings.Default.LogError)
+            {
+                using (var log = new StreamWriter("errorlog.txt", true))
+                {
+                    log.WriteLine("--------------------{0} UTC--------------------", DateTime.UtcNow);
+                    log.WriteLine(ex.ToString());
+                }
+            }
+        }
+    }
+}
